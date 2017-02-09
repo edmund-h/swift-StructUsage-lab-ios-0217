9e37aeef63485ed8ac485af72af50e22c88360e1
edmund-h <447ght@gmail.com>
2017-02-09 16:31:51 -0500

    this was fun~~~

--- AdventureTime/Character.swift
+++ AdventureTime/Character.swift
@@ -9,5 +9,52 @@
 import Foundation
 import UIKit
 
-
+struct Character {
+    let name: String
+    let species: String
+    let occupation: String
+    var powerLevel: Double
+    var killedEnemies: Int = 0
+    var bestFriend: String = ""
+    
+    init (name nameIn: String, species specIn: String, occupation occIn: String, powerLevel plIn: Double){
+        self.name = nameIn
+        self.species = specIn
+        self.occupation = occIn
+        self.powerLevel = plIn
+    }
+    
+    mutating func fightEnemy (character: Character){
+        guard self.powerLevel > character.powerLevel else{
+            return
+        }
+        self.powerLevel -= 1
+        self.killedEnemies += 1
+    }
+    
+    mutating func becomeBestFriendsWith (character: Character) {
+        self.bestFriend = character.name
+    }
+    
+    func displayImage() -> UIImage {
+        
+        switch name {
+        case "Finn the Human": 
+            return UIImage(named: "Finn")!
+        case "Jake the Dog":
+            return UIImage(named: "JakeTheDog")!
+        case "Princess Bubblegum":
+            return UIImage(named: "PrincessBubblegum")!
+        case "Lemongrab":
+            return UIImage(named: "Lemongrab")!
+        case "BMO":
+            return UIImage(named: "BMO")!
+        case "Lumpy Space Princess":
+            return UIImage(named: "LumpySpacePrincess")!
+        default:
+            return UIImage()
+        }
+        
+    }
+}
    
--- AdventureTime/ViewController.swift
+++ AdventureTime/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var occupationLabel: UILabel!
+    @IBOutlet weak var speciesLabel: UILabel!
+    @IBOutlet weak var bigImageView: UIImageView!
+    var charDict: [String : Character] = [:]
+    
+    
+    var finn: Character!
+    var jake: Character!
+    var bubblegum: Character!
+    var bmo: Character!
+    var lemongrab: Character!
+    var lsp: Character!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        createAllCharacters()
     }
-
-  
     
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if let myCharacter = charDict[sender.titleLabel!.text!]{
+        updateViews(with: myCharacter)
+        }// passes button text to updateViews based on dictionary rather than switch statement
+    }
     
-   
-
+    func createAllCharacters() {
+        finn = Character (name: "Finn the Human", species: "Human", occupation: "Hero", powerLevel: 50.0)
+        jake = Character (name: "Jake the Dog", species: "Stretchy Talking Dog", occupation: "Sidekick", powerLevel: 60.0)
+        bmo = Character (name: "BMO", species: "Robot", occupation: "Handheld Gaming System ", powerLevel: 35.0)
+        bubblegum = Character(name: "Princess Bubblegum", species: "Candy Person", occupation: "Regent", powerLevel: 190.0)
+        lemongrab = Character(name: "Lemongrab", species: "Candy Person", occupation: "Autocrat", powerLevel: 49.0)
+        lsp = Character(name: "Lumpy Space Princess", species: "Space Candy Person", occupation: "Princess?", powerLevel: 42)
+        
+        charDict = [
+            "Finn" : finn,
+            "Jake" : jake,
+            "BMO" : bmo,
+            "Bubblegum" : bubblegum,
+            "Lemongrab" : lemongrab,
+            "LSP" : lsp
+        ] //i prefer a dictionary lookup over a switch statement, also i want to get more comfortable with dictionaries and calling from them
+    }
+    
+    func updateViews(with: Character){
+        bigImageView.image = with.displayImage()
+        nameLabel.text = with.name
+        occupationLabel.text = with.occupation
+        speciesLabel.text = with.species
+    }
 }
 
